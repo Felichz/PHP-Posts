c0454ff71aea0164328017e7da80e7d3ab75c94b
Felix Andersson <andersson.fx@gmail.com>
2019-10-11 00:19:33 -0300

    Borradas clases innecesarias

--- lib/Controller/indexController.php
+++ lib/Controller/indexController.php
@@ -7,21 +7,6 @@ class indexController {
 
     public function ejecutarIndexController(){
         $twig = TwigVistas::obtenerTwig();
-
-        //Post de Felix
-        $felix = new \App\Model\Usuario('Felix', '12345');
-        $model_post = new \App\Model\Post();
-        $model_post -> escribirPost($felix -> usuario);
-
-        //Post de Fulanito
-        $liz = new Usuario('Fulanito', '54321');
-        $controller_post = new Post();
-        $controller_post -> escribirPost($liz -> usuario);
-
-        //Saludos
-        echo '<hr/>';
-        $felix -> saludar();
-        $liz -> saludar();
         
         //Cargar autores de los post de la BD
         $bd_post = new \App\Model\BDPosts();
